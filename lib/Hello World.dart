602e63a55cedccf445ce33092a0d0c11dd7d9755
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-04-30 21:19:55 +0300

    1

--- lib/Hello World.dart
+++ lib/Hello World.dart
@@ -7,6 +7,43 @@ class HelloWorld extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+appBar: AppBar(
+backgroundColor: Colors.cyan,
+
+),
+          drawer: Drawer(
+   child:  Column(
+     children: [
+       DrawerHeader(
+         child: Text("Drawer"),
+       ),
+       Row(
+         children: [
+
+         Icon(Icons.home),
+           Text("Home"),
+         ],
+       ),
+       Row(
+         children: [
+         Icon(Icons.favorite),
+           Text("Favourite"),
+         ],
+       ),
+       Row(
+         children: [
+         Icon(Icons.logout),
+           Text("Log out"),
+         ],
+       )
+     
+     ],
+   ),
+
+
+
+          ),
+
 
           body:Column(children: [
             Row(children: [Icon(Icons.cabin),Icon(Icons.cabin)],),
