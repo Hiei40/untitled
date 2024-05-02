7f54f9d7cce8cc2fbd14ac03b4028e10ffa29652
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-05-02 21:29:18 +0300

    Kennyh omega

--- lib/Hello World.dart
+++ lib/Hello World.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HelloWorld extends StatelessWidget {
   const HelloWorld({super.key});
@@ -64,28 +65,72 @@ class HelloWorld extends StatelessWidget {
         ),
 
         body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 50 ,horizontal: 50),
-          child: Column(children: [
-            Stack(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-
-                Container(color: Colors.yellow,
-                  width: double.infinity,
-                  height: 50,),
-
-                Container(child: Image.network(""),),
-
-                Container(
-                  color: Colors.black,
-                  width: double.infinity,
-                  height: 10,
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Image.asset("assets/img.png"),
+                    Container(
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      color: Colors.black.withOpacity(0.4),
+                      child: Text(
+                        "Kenny Omega",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+            
                 ),
+                SizedBox(
+                  height: 50,
+                ),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Image.network("https://static.tvtropes.org/pmwiki/pub/images/dds4x28_2f1d8cd2_526e_4994_b8d9_82515a660af0_5.jpg"),
+                    Container(
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      color: Colors.black.withOpacity(0.4),
+                      child: Text(
+                        "Kenny Omega",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+            
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Image.network("https://monthlypuroresu.com/wp-content/uploads/2021/04/Ospreay_ProvenKingping.jpg"),
+                    Container(
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      color: Colors.black.withOpacity(0.4),
+                      child: Text(
+                        "Will osperay",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+            
+                ),
+                SvgPicture.asset("assets/Google.svg",width: 60,
+                height: 100,),
+
+
+
               ],
-
-            )
-
-
-          ],),
+            ),
+          ),
         ),
       ),
     );
